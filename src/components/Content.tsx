eab65899383a48a5592d2086208f8dea9fd59df9
Juan Villota <juanvillota33@gmail.com>
2024-01-01 18:01:15 -0500

    ajustes en vista

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,7 +4,7 @@ const Content = () => {
     return (
         <>
             <h1 className="w-[100px] text-xl font-bold mb-2">Categorias</h1>
-            <div className="flex-1 h-[100%] w-full flex-col flex items-center z-10" >
+            <div className="flex-1 h-[100%] w-full flex-col flex items-center z-10 overflow-scroll" >
                 <ListCategories />
             </div>
         </>
