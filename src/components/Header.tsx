deed48fc40d6844c209a250f5f578fbdc9defedf
Juan Villota <juanvillota33@gmail.com>
2024-01-01 18:08:36 -0500

    ajuste en titulo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <div className="p-10 flex justify-center items-center h-[15%] w-full">
             <div>
-                <h1 className="text-xl md:text-2xl font-bold ">Convención Nacional de Colombia 2024</h1>
+                <h1 className="text-xl md:text-2xl font-bold text-center">Convención Nacional de Colombia 2024</h1>
                 <h1 className="text-xl md:text-2xl text-center">Guía Turística</h1>
             </div>
         </div>
