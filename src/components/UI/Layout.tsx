4d58f7a9b81b6697c7511877895f801681796667
Juan Villota <juanvillota33@gmail.com>
2024-01-01 18:20:47 -0500

    ajuste en altura y opacidad

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,19 +1,36 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import logocnc from '../../assets/logocnc.svg'
 
 const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
-    const { innerHeight: height } = window;
+    // const { innerHeight: height } = window;
+    const [screenSize, getDimension] = useState({
+        dynamicWidth: window.innerWidth,
+        dynamicHeight: window.innerHeight
+    });
+    const setDimension = () => {
+        getDimension({
+            dynamicWidth: window.innerWidth,
+            dynamicHeight: window.innerHeight
+        })
+    }
 
+    useEffect(() => {
+        window.addEventListener('resize', setDimension);
+
+        return (() => {
+            window.removeEventListener('resize', setDimension);
+        })
+    }, [screenSize])
 
 
     return (
-        <div style={{ height: height - 40 }} className="relative w-full flex flex-col items-center bg-gray-900 text-white overflow-hidden">
+        <div style={{ height: screenSize.dynamicHeight }} className="relative w-full flex flex-col items-center bg-gray-900 text-white overflow-hidden">
             <div className="absolute top-[30%] rotate-12 opacity-[.05] z-1">
                 <img src={logocnc} alt="" width={400} />
             </div>
 
             <div className="glow-left"></div>
-            <div className="glow-right opacity-30 md:opacity-100"></div>
+            <div className="glow-right opacity-60 md:opacity-100"></div>
             {children}
         </div>
     )
