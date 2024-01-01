a08f241ca0a997ebe147f21a857ac4a7f0dbf01f
Juan Villota <juanvillota33@gmail.com>
2024-01-01 17:57:14 -0500

    ajuste opacidad logo

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -4,7 +4,7 @@ import logocnc from '../../assets/logocnc.svg'
 const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
     return (
         <div className="relative w-full flex flex-col items-center bg-gray-900 text-white overflow-hidden">
-            <div className="absolute top-[30%] rotate-12 opacity-[.01] z-1">
+            <div className="absolute top-[30%] rotate-12 opacity-[.05] z-1">
                 <img src={logocnc} alt="" width={400} />
             </div>
 
