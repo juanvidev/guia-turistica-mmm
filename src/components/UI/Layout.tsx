667eb794421992def31f36bf2ffc86c46635a9c7
Juan Villota <juanvillota33@gmail.com>
2024-01-01 17:52:38 -0500

    ajsute en altura

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -3,7 +3,7 @@ import logocnc from '../../assets/logocnc.svg'
 
 const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
     return (
-        <div className="relative h-dvh w-full flex flex-col items-center bg-gray-900 text-white overflow-hidden min-w-96 min-h-dvh">
+        <div className="relative w-full flex flex-col items-center bg-gray-900 text-white overflow-hidden">
             <div className="absolute top-[30%] rotate-12 opacity-[.01] z-1">
                 <img src={logocnc} alt="" width={400} />
             </div>
