0670be588e9711c11a8ecac50041a8123f4c0a60
Juan Villota <juanvillota33@gmail.com>
2024-01-01 19:07:06 -0500

    ajuste altura en header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import LogoMMM from '../assets/logommm.png'
 
 const Header = () => {
     return (
-        <div className="px-10 flex flex-col pt-4 justify-center items-center h-[25%] w-full">
+        <div className="px-10 flex flex-col pt-4 justify-center items-center h-[22%] w-full">
             <img className='mb-5 object-contain' width={60} src={LogoMMM} alt="LogoMMM" />
             <h1 className="text-md md:text-2xl text-center">Convención Nacional de Colombia 2024</h1>
             <h1 className="text-3xl md:text-4xl font-bold text-center">Guía Turística</h1>
