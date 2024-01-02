8f87dc8a96c37a5d4ea80250d949e168f7b777c7
Juan Villota <juanvillota33@gmail.com>
2024-01-01 22:46:38 -0500

    ajuste en place

--- src/components/UI/ListOfPlaces.tsx
+++ src/components/UI/ListOfPlaces.tsx
@@ -24,23 +24,23 @@ const ListOfPlaces: FC = () => {
                             d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                     </svg>
                 </button>
-                <h2 className="text-lg w-full text-left font-normal text-white">{categoryName}</h2>
+                <h2 className="text-md w-full text-left font-normal text-white">{categoryName}</h2>
             </div>
             <div className="grid grid-cols-2 gap-x-2 gap-y-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 
                 {
                     places.map((place: PlaceProps) =>
-                        <a target="_blank" key={place.place_name} href={place.ubication} className="group ease-in-out duration-300 hover:duration-300 hover:bg-gray-600/30">
-                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                        <a target="_blank" key={place.place_name} href={place.ubication} className="group rounded-lg border-[1px] border-slate-500/10 ease-in-out duration-300 hover:duration-300 hover:bg-gray-600/30">
+                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                 <img
                                     width={200}
                                     height={200}
                                     src={place.imageUrl}
                                     alt={place.place_name || ""}
-                                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                                    className="h-full w-full object-cover object-center group-hover:opacity-75 group-hover:duration-300 ease-in-out duration-300"
                                 />
                             </div>
-                            <h3 className="mt-4 text-sm font-semibold text-white p-2">{place.place_name}</h3>
+                            <h3 className="text-md font-semibold text-white p-2">{place.place_name}</h3>
                             {/* <p className="mt-1 text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta explicabo, molestias consequatur eaque.</p> */}
                         </a>
                     )
