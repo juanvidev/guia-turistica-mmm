f01c9b33c5c0218c88e44cb3bb47a5a0e1356692
Juan Villota <juanvillota33@gmail.com>
2024-01-04 00:30:58 -0500

    cambios en interfaz

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,28 +15,26 @@ const Header = () => {
 
     return (
         <>
-            <div className="px-10 flex flex-col pt-4 justify-center items-center h-[24%] w-full z-10">
-                <img className='mb-5 object-contain' width={60} src={LogoMMM} alt="LogoMMM" />
+            <div className="px-10 flex flex-col pt-4 justify-center items-center h-[22%] w-full z-10">
+                <img className='mb-5 object-contain' width={50} src={LogoMMM} alt="LogoMMM" />
                 <h1 className="text-md md:text-2xl text-center">Convención Nacional de Colombia 2024</h1>
                 <h1 className="text-3xl md:text-4xl font-bold text-center">Guía Turística</h1>
                 <I18nWidget />
             </div>
-            <div className="flex w-full sm:w-[40%] z-10 px-5">
-                {
-                    location.pathname !== '/' && (
-                        <>
-                            <button className="mr-5" onClick={() => handleBack()}>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                        d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                                </svg>
-                            </button>
-                            <h2 className="text-md w-full text-left font-normal text-white">{state ? state.category : ''}</h2>
-                        </>
-                    )
-                }
-            </div>
+            {
+                location.pathname !== '/' && (
+                    <div className="flex w-full sm:w-[40%] z-10 px-5 py-2">
+                        <button className="mr-5" onClick={() => handleBack()}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                    d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                            </svg>
+                        </button>
+                        <h2 className="text-md w-full text-left font-normal text-white">{state ? state.category : ''}</h2>
+                    </div>
+                )
+            }
 
         </>
     )
