523958d1423b463fbc087f594b969ecde185263b
Juan Villota <juanvillota33@gmail.com>
2024-01-04 22:28:48 -0500

    CAMBIOS finales

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import logocnc from '../assets/logocnc.svg'
 const Footer = () => {
     const { i18n } = useTranslation();
     return (
-        <div className="flex justify-center w-full h-[15%]">
+        <div className="flex justify-center w-full h-[10%]">
             <img className='md:w-[60px]' src={logocnc} alt="LogoCNC2024" width={50} />
             {i18n.language === 'es' ? <img className='md:w-[200px] ml-5' src={lema} alt="Lema2024" width={200} /> : <img className='md:w-[200px] ml-5' src={lemaEN} alt="Lema2024" width={200} />}
         </div>
