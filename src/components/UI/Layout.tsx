3545ad755d880915589df4c31f8fa974b4e15c85
Juan Villota <juanvillota33@gmail.com>
2024-01-06 01:15:08 -0500

    ajuste en pantalla carga

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -4,8 +4,8 @@ import img from '../../assets/fondocnc.svg';
 
 const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
-    // const [isLoaded, setIsLoaded] = useState(false);
-
+    const [isLoaded, setIsLoaded] = useState(false);
+    // const refImg = useRef<HTMLImageElement>(null);
     // const { innerHeight: height } = window;
     const [screenSize, getDimension] = useState({
         dynamicWidth: window.innerWidth,
@@ -13,9 +13,10 @@ const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => 
     });
 
 
-    // const handleLoad = () => {
-    //     setIsLoading(false)
-    // }
+    const handleLoad = () => {
+        setIsLoaded(true)
+    }
+    console.log(isLoaded)
 
     const setDimension = () => {
         getDimension({
@@ -24,30 +25,41 @@ const Layout: FC<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => 
         })
     }
     useLayoutEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 1000)
+
+        const timeout = setTimeout(() => {
+            setIsLoading(isLoaded ? false : true)
+        }, 1300);
+
         window.addEventListener('resize', setDimension);
-        return () => window.removeEventListener('resize', setDimension);
-    }, [])
 
-    return isLoading ? (
-        <div style={{ height: screenSize.dynamicHeight }} className="bg-gray-900 text-white w-full">
-            <h1>Cargando</h1>
-        </div>
+        return () => {
+            window.removeEventListener('resize', setDimension);
+            clearTimeout(timeout)
+        }
+    }, [isLoaded])
 
-    ) : (
-        <div style={{ height: screenSize.dynamicHeight }} className="relative w-full flex flex-col items-center bg-gray-900 overflow-hidden">
-            <div className="absolute top-[30%] rotate-12 opacity-[.05] z-1">
-                <img src={logocnc} alt="" width={300} />
-            </div>
+    return (
+        <div className="relative bg-gray-900 w-full h-full">
+            <img src={img} onLoad={handleLoad} className={`absolute w-full object-cover z-[0] h-full block min-[641px]:hidden ${isLoading ? 'hidden' : 'block'}`} alt="FondoCNC" />
+            {isLoading ? (
+                <div style={{ height: screenSize.dynamicHeight }} className="bg-gray-900 text-white w-full">
+                    <h1>Cargando</h1>
+                </div>
 
-            <div className="glow-left z-[5]"></div>
-            <div className="glow-right z-[5] opacity-60 md:opacity-100"></div>
+            ) : (
+                <div style={{ height: screenSize.dynamicHeight }} className="relative w-full flex flex-col items-center overflow-hidden">
+                    <div className="absolute top-[30%] rotate-12 opacity-[.05] z-1">
+                        <img src={logocnc} alt="" width={300} />
+                    </div>
 
-            <img src={img} loading="lazy" className="absolute w-full object-cover z-[0] h-full block min-[641px]:hidden" alt="" />
+                    <div className="glow-left z-[5]"></div>
+                    <div className="glow-right z-[5] opacity-60 md:opacity-100"></div>
 
-            {children}
+
+                    {children}
+                </div>
+            )}
+
         </div>
     )
 }
