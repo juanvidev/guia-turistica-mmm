c75cbf660aacf5eb5b2022d9cb60b6d0faff80a8
Juan Villota <juanvillota33@gmail.com>
2024-01-05 20:27:53 -0500

    Actualizar App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -40,7 +40,10 @@ const App = () => {
         <Route path="/:category" element={<ListOfItems />} />
 
 
-<Redirect from='*' to='/' />
+<Route
+        path="*"
+        element={<Navigate to="/" replace />}
+    />
       </Routes>
       <Footer />
     </Layout>
